a8ff10ba8d98666f32ad722a6ced98271a7cb233
peterk <tiglek+u16office@gmail.com>
2021-07-09 20:40:46 +0900

    - impl github download

--- bin/node-init.js
+++ bin/node-init.js
@@ -2,17 +2,33 @@
 
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
+
+const { downloadFromGithub } = require("../lib/github");
+
 const argv = yargs(hideBin(process.argv))
-.usage("Usage: $0 [options] [git-repo-url]")
-.example("$0 https://github.com/aistyler/node-init.git")
-.alias("o", "options")
-.describe("o", "options for init")
-.demandCommand(1)
-.help('h')
-.epilog('copyright 2019')
-.argv;
+              .usage("Usage: $0 [options] [git-repo-url]")
+              .example("$0 github:aistyler/node-init/main")
+              .options({
+                "f": {
+                  alias: "force",
+                  default: false,
+                  describe: "Overwrite the existing files",
+                  type: "boolean",
+                },
+                "o": {
+                  alias: "outdir",
+                  default: "./",
+                  describe: "Output directory",
+                  type: "string",
+                }
+              })
+              .demandCommand(1)
+              .help('h')
+              .epilog('copyright 2019')
+              .argv;
 
-console.log(">>>", argv);
-
-
-
+//console.log(argv);
+(async() => await downloadFromGithub(argv._[0], {
+  force: argv.force,
+  outdir: argv.outdir,
+}))();
